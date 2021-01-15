8ef609f73949f88244d26aa451264611b0586fe1
andrewsilent <36265247+andrewsilent@users.noreply.github.com>
2021-01-15 14:42:31 +0200

    fix default values, validators, array

--- script.js
+++ script.js
@@ -3,30 +3,30 @@
  * @param {number} from - The range.from value, default = 0..-1000
  * @param {number} to - The range.to value, default = 0..1000
  */
-function createRandomValue(n = 100) {
-  return Math.ceil(Math.random() * (n));
-}
 class RangeValidator {
-  constructor(from = createRandomValue(0), to = createRandomValue(10)) {
-    if (!(Number.isInteger(+from)) || !(Number.isInteger(+to))) {
+  constructor(from = 0, to = 10) {
+    from = Number(from);
+    to = Number(to);
+    if (!(Number.isInteger(from)) || !(Number.isInteger(to))) {
       throw new TypeError('Wrong type of value, {number} expected');
     }
-    if (+to < +from) {
+    if (to < from) {
       throw new RangeError('Value "To" must be greater than value "From"');
     }
-    this._from = +from;
-    this._to = +to;
+    this._from = from;
+    this._to = to;
   }
 
   set from(v) {
-    if (Number.isInteger(+v)) {
-      this._from = +v;
-      return;
-    }
-    if (+v > this._to) {
+    v = Number(v);
+    if (v > this._to) {
       throw new RangeError('Value "From" must be less than value "To"');
     }
-    throw new TypeError('Wrong type of value, {number} expected');
+    if (!Number.isInteger(v)) {
+      throw new TypeError('Wrong type of value, {number} expected');
+    }
+    this._from = v;
+    return;
   }
 
   get from() {
@@ -34,14 +34,15 @@ class RangeValidator {
   }
 
   set to(v) {
-    if (Number.isInteger(+v)) {
-      this._to = +v;
-      return;
-    }
-    if (+v < this._from) {
+    v = Number(v);
+    if (v < this._from) {
       throw new RangeError('Value "To" must be greater than value "From"');
     }
-    throw new TypeError('Wrong type of value, {number} expected');
+    if (!Number.isInteger(v)) {
+      throw new TypeError('Wrong type of value, {number} expected');
+    }
+    this._to = v;
+    return;
   }
 
   get to() {
@@ -49,14 +50,18 @@ class RangeValidator {
   }
 
   get range() {
-    return Array(this._from, this._to);
+    return [this._from, this._to];
   }
 
   validate(n) {
-    if ((n - this._from) * (n - this._to) <= 0) {
-      return n;
+    n = Number(n);
+    if (!Number.isInteger(n)) {
+      throw new TypeError('Wrong type of value, {number} expected');
     }
-    throw new RangeError(`${n} is out of range ${this._from}..${this._to}`);
+    if ((n - this._from) * (n - this._to) > 0) {
+      throw new RangeError(`${n} is out of range ${this._from}..${this._to}`);
+    }
+    return n;
   }
 }
 
